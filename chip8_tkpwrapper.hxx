44285bd4c11568577be58f8c7b008572a7c03c8a
offtkp <parisoplop@gmail.com>
2022-05-27 02:59:08 +0300

    tkpwrapper initial commit

---
+++ chip8_tkpwrapper.hxx
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef TKP_CHIP8_TKPWRAPPER_H
+#define TKP_CHIP8_TKPWRAPPER_H
+#include "../include/emulator.h"
+
+namespace TKPEmu::Chip8 {
+    class Chip8 : public Emulator {
+
+    };
+}
+#endif
