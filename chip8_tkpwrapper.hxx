be4ac97a4ab8554ac5829c33ff16c0a6f2eca52a
offtkp <parisoplop@gmail.com>
2022-05-28 22:02:24 +0300

    add screen_color_data_

--- chip8_tkpwrapper.hxx
+++ chip8_tkpwrapper.hxx
@@ -2,10 +2,12 @@
 #ifndef TKP_CHIP8_TKPWRAPPER_H
 #define TKP_CHIP8_TKPWRAPPER_H
 #include <include/emulator.h>
+#include <include/TKPImage.h>
 
 namespace TKPEmu::Chip8 {
     class Chip8 : public Emulator {
 
+		std::array<float, 4 * 64 * 32> screen_color_data_{};
     };
 }
 #endif
