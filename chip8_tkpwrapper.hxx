4903fa317b12a9bf7eca580c59d4a04fa73590c8
offtkp <parisoplop@gmail.com>
2022-07-02 01:22:52 +0300

    Fix identation. Most work im willing to put on chip8 rn

--- chip8_tkpwrapper.hxx
+++ chip8_tkpwrapper.hxx
@@ -8,11 +8,11 @@
 namespace TKPEmu::Chip8 {
 	using Chip8Keys = std::array<SDL_Keycode, 16>;
     class Chip8 : public Emulator {
-		TKP_EMULATOR(Chip8);
+        TKP_EMULATOR(Chip8);
     private:
         Interpreter inter_;
-		void update();
-		Chip8Keys key_mappings_;
+        void update();
+        Chip8Keys key_mappings_;
     };
 }
 #endif
